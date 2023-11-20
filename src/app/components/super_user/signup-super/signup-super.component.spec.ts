b7bac1555631f8e6439b33fda71e456eda0be2f5
AlexValenz <valenzuela1235@gmail.com>
2023-11-20 17:16:03 -0600

    add singup_super cmpnt

---
+++ src/app/components/super_user/signup-super/signup-super.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SignupSuperComponent } from './signup-super.component';
+
+describe('SignupSuperComponent', () => {
+  let component: SignupSuperComponent;
+  let fixture: ComponentFixture<SignupSuperComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SignupSuperComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(SignupSuperComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/super_user/signup-super/signup-super.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-signup-super',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './signup-super.component.html',
+  styleUrl: './signup-super.component.css'
+})
+export class SignupSuperComponent {
+
+}
